68e05221b24bd772837374f23f1325427fede3b8
fstipo <fstipanov@gmail.com>
2022-05-23 18:15:21 +0200

    add collapsed class

--- 01. Navigation-bootstrap/app.js
+++ 01. Navigation-bootstrap/app.js
@@ -1,1 +1,7 @@
-console.log('Hello');
+const collapseBtn = document.querySelector('.navigation__collapse-button');
+const navigationEl = document.querySelector('.section__navigation');
+const navigationBox = document.querySelector('.navigation__box');
+
+collapseBtn.addEventListener('click', () => {
+  navigationEl.classList.toggle('collapsed');
+});
